2cb3f251b87bec57cc2392063977cfe7ba1c9b47
Johan Hennius <johan.bh19@gmail.com>
2023-10-18 12:51:39 +0000

    Initial commit

--- arrow-functions.js
+++ arrow-functions.js
@@ -5,10 +5,39 @@
 
 // Vanilla JavaScript Function
 
+// function addTwoNumbers (a, b) {
+//     return a + b;
+// }
+
+// let sum = addTwoNumbers(7, 10);
+// console.log(sum);
+
 // Arrow Function With Parameters
+
+const addTwoNumbers = (a, b)  => {
+    return a + b;
+}
+
+let sum = addTwoNumbers(7, 10);
+console.log(sum);
 
 // Single Line Arrow Function With Parameters
 
+const addTwoNumbers2 = (a, b) => a + b;
+
 // Implicit Returns
 
+const saySomething = message => console.log(message);
+saySomething("Hello!")
+
+const sayHello = () => console.log("Hiya");
+sayHello();
+
 // Returning Multiple Lines
+
+const returnMultipleLines = () => (
+    `
+    <p>You've got a new message</p>
+    `
+)
+console.log(returnMultipleLines());
